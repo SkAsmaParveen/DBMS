db9e16311c0c7a7d5c76eb06de37e4c767fe7856
SHAIK ASMA PARVEEN <112798058+SkAsmaParveen@users.noreply.github.com>
2023-04-19 10:52:54 +0530

    Update week3.sql

--- week3.sql
+++ week3.sql
@@ -1,3 +1,6 @@
+1. Find all possible combinations of instructors and the courses they teach.
+
+
 SQL> select name,course_id from instructor,teaches where instructor.ID = teaches.id;
 
 NAME                 COURSE_I
@@ -21,4 +24,32 @@ Brandt               CS-319
 Kim                  EE-181
 
 14 rows selected.
+
+
+2. Retrieve the names of all instructors, along with their department names and
+   department building name.
+
+SQL> select name,instructor.dept_name,building from instructor,department where instructor.dept_name
+ = department.dept_name;
+
+NAME                 DEPT_NAME            BUILDING
+-------------------- -------------------- ---------------
+Srinivasan           Comp. Sci.           Taylor
+Wu                   Finance              Painter
+Mozart               Music                Packard
+Einstein             Physics              Watson
+El Said              History              Painter
+Gold                 Physics              Watson
+Katz                 Comp. Sci.           Taylor
+Califieri            History              Painter
+Singh                Finance              Painter
+Crick                Biology              Watson
+Brandt               Comp. Sci.           Taylor
+
+NAME                 DEPT_NAME            BUILDING
+-------------------- -------------------- ---------------
+Kim                  Elec. Eng.           Taylor
+
+12 rows selected.
+
 
