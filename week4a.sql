b230aff489cf7dc4fd4271d3c14235de1e702b25
SHAIK ASMA PARVEEN <112798058+SkAsmaParveen@users.noreply.github.com>
2023-04-27 13:59:03 +0530

    Update week4a.sql

--- week4a.sql
+++ week4a.sql
@@ -188,5 +188,15 @@ ID    TOTALCREDITS
 98988            8
 
 12 rows selected.
+                                                      
+                                                      SQL> select count(distinct takes.id) as TotalStudents from takes ,teaches t where takes.course_id=t
+  2  .course_id AND t.id=10101 group by t.course_id,t.sec_id;
+
+TOTALSTUDENTS
+-------------
+            6
+            2
+            2
+
                                                     
                                                       
