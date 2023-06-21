d1dcc26fa4ba0e3aed5e4eff9ec01fecb193d6e1
SHAIK ASMA PARVEEN <112798058+SkAsmaParveen@users.noreply.github.com>
2023-06-21 10:06:29 +0530

    Update multiply.sql

--- multiply.sql
+++ multiply.sql
@@ -13,3 +13,24 @@ end loop;
 end;
 
 
+
+SQL> @z:\plsql\multiply.sql
+ 16  /
+Enter value for n: 2
+old   6: n:=&n;
+new   6: n:=2;
+2*1=2
+2*2=4
+2*3=6
+2*4=8
+2*5=10
+2*6=12
+2*7=14
+2*8=16
+2*9=18
+2*10=20
+
+PL/SQL procedure successfully completed.
+
+
+
