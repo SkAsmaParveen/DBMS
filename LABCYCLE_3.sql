f00009d6c8aeb9fc05c9ef0fadf180828d360c4c
SHAIK ASMA PARVEEN <112798058+SkAsmaParveen@users.noreply.github.com>
2023-06-28 14:36:59 +0530

    Update LABCYCLE_3.sql

--- LABCYCLE_3.sql
+++ LABCYCLE_3.sql
@@ -69,4 +69,65 @@ PL/SQL procedure successfully completed.
 
 
 2.Write a PL/SQL block which will accept two numbers and find out their LCM and HCF. The output should be stored in a table called DEMO_TAB.
+  -- should create a table named DEMO_TAB before hand
+-- CREATE TABLE DEMO_TAB (
+--     Num1    numeric(4,0),
+--     Num2    numeric(4,0),
+--     LCM     numeric(2,0),
+--     HCF     numeric(2,0)
+-- );
+declare
+    n1 DEMO_TAB.Num1%type := &n1;
+    n2 DEMO_TAB.Num2%type := &n2;
+    ma number;
+    mi number;
+    gcd DEMO_TAB.HCF%type;
+    lcm DEMO_TAB.LCM%type;
+    i number;
+    r DEMO_TAB%ROWTYPE;
+begin
+    if n1>n2 then
+    ma := n1;
+    mi := n2;
+    else
+    mi := n1;
+    ma := n2;
+    end if;
+    for i in 2..mi loop
+    if MOD(mi, i)=0 AND MOD(ma, i)=0 then   
+    lcm := i;
+    EXIT;
+    end if;
+    end loop;
+
+    for i in REVERSE 1..mi loop
+    if MOD(mi, i)=0 AND MOD(ma, i)=0 then
+    gcd := i;
+    EXIT;
+    end if;
+    end loop;
+
+    INSERT INTO DEMO_TAB VALUES(n1, n2, lcm, gcd);
+end;
+
+
+OUTPUT:
+  
+SQL> @D:\PLSQL\DEMOTAB.SQL
+ 34  /
+Enter value for n1: 2
+old   2:     n1 DEMO_TAB.Num1%type := &n1;
+new   2:     n1 DEMO_TAB.Num1%type := 2;
+Enter value for n2: 4
+old   3:     n2 DEMO_TAB.Num2%type := &n2;
+new   3:     n2 DEMO_TAB.Num2%type := 4;
+
+PL/SQL procedure successfully completed.
+
+SQL> SELECT * from demo_tab;
+
+      NUM1       NUM2        LCM        HCF
+---------- ---------- ---------- ----------
+         2          4          2          2
+
 
